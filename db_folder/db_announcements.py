8016056f98e56cff980c1accfd4539afd54c5ea1
slavi <slavikk2010@inbox.ru>
2026-02-03 22:45:39 +0300

    а ещё я не умею работать с db

--- db_folder/db_announcements.py
+++ db_folder/db_announcements.py
@@ -14,8 +14,8 @@ class AnnouncementsRepository:
 
         await self.db.execute(
             f"""
-                INSERT INTO {self.__TABLE}
-                WHERE channel_id = ?
+                INSERT INTO {self.__TABLE} (channel_id)
+                VALUES (?)
                 ON CONFLICT (channel_id) DO NOTHING
             """,
             (channel_id,)
