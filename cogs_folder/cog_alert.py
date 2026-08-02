8f9eacb48b27788ddaf929b125070856a3bf5af3
slavi <slavikk2010@inbox.ru>
2026-08-02 15:36:13 +0300

    добавлен keep-alive

--- cogs_folder/cog_alert.py
+++ cogs_folder/cog_alert.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import json
 from configs_folder.advanced_settings import ALERT_CHANNEL_ID
 from cryptography.fernet import Fernet
-from services_folder.srv_alert import start_alert
+from services_folder.srv_alert import start_alert, keep_alive
 CONFIGS_FODLER = Path(__file__).with_name("configs_folder")
 SETTINGS_PATH = CONFIGS_FODLER / "settings.json"
 
@@ -15,6 +15,13 @@ with open(SETTINGS_PATH, "r", encoding="utf-8") as f:
 
 ALERT_KEY = config_setings["ALERT_KEY"]
 ALERT_TEXT = config_setings["ALERT_TEXT"]
+users_ids = [
+    1350862362290294886,
+    1148302655601520690,
+    759310706343542854,
+    1107016230499536936,
+    1160688626934497481
+]
 class alert(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
@@ -22,7 +29,8 @@ class alert(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if message.channel.id != ALERT_CHANNEL_ID:
+        channel = message.channel
+        if channel.id != ALERT_CHANNEL_ID:
             return
         try:
             cipher = Fernet(ALERT_KEY.encode())
@@ -33,16 +41,28 @@ class alert(commands.Cog):
 
         if decrypted == ALERT_TEXT:
             await start_alert(self.bot)
-            await message.delete()
+            await channel.send("202")
+        else:
+            if await keep_alive(self.bot, decrypted):
+                await channel.send("200")
+            else:
+                await channel.send("400")
+        await message.delete()
 
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
         if member.guild.id != 1370157504931172473:
             return
-        if member.id == 1350862362290294886:
+        if member.id not in users_ids:
             await member.kick()
             await member.send("Недостаточно прав для вступления на этот сервер.")
+
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await keep_alive(self.bot, "")
+
 
 
 async def setup(bot: Bot):
